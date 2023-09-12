305ffade7b6f3b7a1797cf78bd1fd8d32c26f754
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2023-09-12 16:09:52 -0300

    Testes Iterasys

--- src/test/java/apiTest/testeUser.java
+++ src/test/java/apiTest/testeUser.java
@@ -14,6 +14,8 @@ import static org.hamcrest.Matchers.is;
 // Classe
 public  class testeUser {
 // Atributos
+    String ct = "application/Json";                            // Content Type
+    String uriUser = "https://petstore.swagger.io/v2/user/";
 // Funções e Métodos
 // Funções de Apoio
     public static String lerArquivoJson(String arquivoJson) throws IOException {
@@ -29,18 +31,38 @@ public  class testeUser {
 
 // Realizar o Ieste
     given()                                                        // Dado que
-            .contentType("application/json")           // o tipo do conteúdo
+            .contentType(ct)           // o tipo do conteúdo
             .log().all()                                        // mostre tudo
             .body(jsonBody)                             // corpo da requisição
-            .when()                                                  // Quando
+    .when()                                                          // Quando
             .post("https://petstore.swagger.io/v2/user")  // Endpoint/ Onde
-            .then()                                                   // Então
+    .then()                                                           // Então
             .log().all()                             // mostre tudo no retorno
             .statusCode(200)                    // Comunicação - ida e volta
             .body("code", is(200))                   // Tag Code é 200
             .body("type", is("unknown"))         // Tag type é unknowm
             .body("message", is(userId))
     ;
-
+    }                                                           // Fim do Post
+    @Test
+    public void testarConsultarUser(){
+        String username = "axel";
+        int userId = 1372633669;                         // Código do usuário
+        String email = "axel@test.com";
+        String senha = "alex1975";
+        String telefone = "989134557";
+    given()
+            .contentType(ct).log().all()
+    .when()
+            .get(uriUser + username)
+    .then()
+            .log().all()
+            .statusCode(200)
+            .body("id", is(userId))
+            .body("email", is(email))
+            .body("password", is(senha))
+            .body("phone", is(telefone))
+        ;
     }
+
 }
