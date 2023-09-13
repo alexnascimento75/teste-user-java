95f34c4d0e651c43705d57eb25a31e7210d8ac40
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2023-09-13 10:14:11 -0300

    Testes Iterasys

--- src/test/java/apiTest/testeUser.java
+++ src/test/java/apiTest/testeUser.java
@@ -2,7 +2,9 @@
 package apiTest;
 
 // Bibliotecas
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,10 +14,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 // Classe
-public  class testeUser {
+
+public class testeUser {
 // Atributos
-    String ct = "application/Json";                            // Content Type
-    String uriUser = "https://petstore.swagger.io/v2/user/";
+static String ct = "application/Json";                            // Content Type
+static String uriUser = "https://petstore.swagger.io/v2/user/";
 // Funções e Métodos
 // Funções de Apoio
     public static String lerArquivoJson(String arquivoJson) throws IOException {
@@ -63,6 +66,42 @@ public  class testeUser {
             .body("password", is(senha))
             .body("phone", is(telefone))
         ;
-    }
+    }                                                       //Fim do Get User
+    @Test
+    public void testarAlterarUser() throws IOException {// Inicio do Put User
+        String jsonBody = lerArquivoJson("src/test/resources/json/user2.json");
+        String userId = "1372633669";
+        String username = "axel";
 
+        given()
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uriUser + username)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(userId))
+        ;
+    }                                                       // Fim da Put User
+
+@Test
+public void testarExcluirUser(){
+        String username = "axel";
+        given()
+                .contentType(ct)
+                .log().all()
+        .when()
+                .delete(uriUser + username)
+        .then()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(username))
+        ;
 }
+
+}                                                             // Fim da Classe
