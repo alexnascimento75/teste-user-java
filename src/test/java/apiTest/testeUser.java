9a355696def026c7d5ac48c2d5a89c23ccb4f051
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2023-09-14 09:27:29 -0300

    Teste User

--- src/test/java/apiTest/testeUser.java
+++ src/test/java/apiTest/testeUser.java
@@ -2,8 +2,11 @@
 package apiTest;
 
 // Bibliotecas
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 import java.io.IOException;
@@ -11,13 +14,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 // Classe
 
 public class testeUser {
 // Atributos
-static String ct = "application/Json";                            // Content Type
+static String ct = "application/Json";                         // Content Type
 static String uriUser = "https://petstore.swagger.io/v2/user/";
 // Funções e Métodos
 // Funções de Apoio
@@ -45,7 +48,7 @@ static String uriUser = "https://petstore.swagger.io/v2/user/";
             .body("code", is(200))                   // Tag Code é 200
             .body("type", is("unknown"))         // Tag type é unknowm
             .body("message", is(userId))
-    ;
+        ;
     }                                                           // Fim do Post
     @Test
     public void testarConsultarUser(){
@@ -54,6 +57,7 @@ static String uriUser = "https://petstore.swagger.io/v2/user/";
         String email = "axel@test.com";
         String senha = "alex1975";
         String telefone = "989134557";
+
     given()
             .contentType(ct).log().all()
     .when()
@@ -91,6 +95,7 @@ static String uriUser = "https://petstore.swagger.io/v2/user/";
 @Test
 public void testarExcluirUser(){
         String username = "axel";
+
         given()
                 .contentType(ct)
                 .log().all()
@@ -103,5 +108,68 @@ public void testarExcluirUser(){
                 .body("message", is(username))
         ;
 }
+@Test
+public void testarLogin(){
+        String username = "axel";
+        String password = "alex1975";
+
+         Response response = (Response) given()
+                .contentType(ct)
+                .log().all()
+        .when()
+                .get(uriUser + "login?username=" + username +"&password=" + password)
+        .then()
+                 .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", containsString("logged in user session:"))
+                .body("message", hasLength(36))
+                .extract()
+        ;
+// Extração do Token da resposta
+    String token = response.jsonPath().getString("message").substring(23);
+    System.out.println("Conteúdo do Token: " + token )
+        ;
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "csv/massaUser.csv", numLinesToSkip = 1, delimiter = ',')
+public void testarIncluirUserCsv(
+        String id,
+        String username,
+        String firstName,
+        String lastName,
+        String email,
+        String password,
+        String phone,
+        String userStatus) {                             //Inio Incluir CSV
+
+        StringBuilder jsonBody = new StringBuilder("{");
+
+        jsonBody.append("'id':" + id + ",");
+                jsonBody.append("'username´: " + username + ",");
+                jsonBody.append("'firstName': " + firstName + ",");
+                jsonBody.append("'lastName': " + lastName + ",");
+                jsonBody.append("'email': " + email + ",");
+                jsonBody.append("'password': " + password + ",");
+                jsonBody.append("'phone': " + phone + ",");
+                jsonBody.append("'userStatus' " + userStatus);
+                jsonBody.append("}");
+
+        given()                                                       // Dado que
+                .contentType(ct)                            // o tipo do conteúdo
+                .log().all()                                       // mostre tudo
+                .body(jsonBody)                            // corpo da requisição
+        .when()                                                         // Quando
+                .post("https://petstore.swagger.io/v2/user")  // Endpoint/ Onde
+        .then()                                                           // Então
+                .log().all()                             // mostre tudo no retorno
+                .statusCode(200)                    // Comunicação - ida e volta
+                .body("code", is(200))                   // Tag Code é 200
+                .body("type", is("unknown"))         // Tag type é unknowm
+                .body("message", is(id))
+        ;
+    }
 
 }                                                             // Fim da Classe
